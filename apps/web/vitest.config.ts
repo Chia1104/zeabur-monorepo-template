0dea194f36f772f6cd8ec8b68dabf46c36087601
Chia1104 <yuyuchia7423@gmail.com>
2024-03-08 18:35:29 +0800

    test serverless

--- apps/web/vitest.config.ts
+++ apps/web/vitest.config.ts
@@ -1,13 +1,12 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   test: {
     environment: "happy-dom",
   },
-  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
